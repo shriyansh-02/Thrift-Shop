8a1b5164e08c94ccb6c9d9d136c1ae51c2c75c93
Shriyansh Singh <shriyanshsingh352@gmail.com>
2026-01-13 14:48:37 +0530

    Update PostCards.js

--- src/Components/PostCards/PostCards.js
+++ src/Components/PostCards/PostCards.js
@@ -1,38 +1,36 @@
-import React, { useContext } from 'react'
+import React,{useContext} from 'react'
 import Heart from '../../assets/Heart'
-import { useHistory } from "react-router-dom"
-import { PostContext } from "../../contextStore/PostContext"
+import {useHistory} from "react-router-dom";
+import {PostContext} from "../../contextStore/PostContext";
 import "./postcards.css"
 
-function PostCards({ product, index }) {
-  const { setPostContent } = useContext(PostContext)
-  const history = useHistory()
+function PostCards({product,index}) {
+    let {setPostContent} = useContext(PostContext)//at the time of onClick on post ,the specified post item assigned to postContent by setPostContent function and it will be stored in a global context PostContext
+ 
+    const history=useHistory()//at the time of onClick on post , we want redirect to the view post page
 
-  const handleClick = () => {
-    setPostContent(product)
-    history.push("/view")
-  }
-
-  return (
-    <div className="card" key={index} onClick={handleClick}>
-      
-      <div className="favorite">
-        <Heart />
+    return (
+      <div className="card" key={index} onClick={()=>{
+        setPostContent(product)
+        history.push("/view")
+      }}>
+        <div className="favorite">
+          <Heart></Heart>
+        </div>
+        <div className="image">
+          <img src={product.url} alt="" />
+        </div>
+        <div className="content">
+          <p className="rate">&#x20B9; {product.price}</p>
+          <span className="category"> {product.category} </span>
+          <p className="name"> {product.name}</p>
+        </div>
+        <div className="date">
+          <span>{product.createdAt}</span>
+        </div>
       </div>
-
-      <div className="image">
-        <img src={product.url} alt={product.name} />
-      </div>
-
-      <div className="content">
-        <p className="rate">&#x20B9; {product.price}</p>
-        <span className="category">{product.category}</span>
-        <p className="name">{product.name}</p>
-      </div>
-
-      {/* Upload date REMOVED – hidden from public users */}
-    </div>
-  )
+       
+    )
 }
 
 export default PostCards
